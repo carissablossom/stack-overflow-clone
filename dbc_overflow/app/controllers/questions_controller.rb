56776ff58479bebd59482a51cd85e3722c3ef97e
Markinator & Nilaco <pair+Makdash49.nilaco@devbootcamp.com>
2015-05-18 17:44:12 -0700

    add question form and create action with redirect.

--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -8,4 +8,22 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(param_check)
+    @question.save
+    redirect_to @question
+  end
+
+  private
+
+  def param_check
+    params.require(:question).permit(:title, :content)
+  end
+
 end
+
+
