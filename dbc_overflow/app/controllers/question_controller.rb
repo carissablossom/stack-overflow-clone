562934c6137ba547fbae7465b9d0d17789439c5a
edisonocean <edisonoceanreef@gmail.com>
2015-05-18 17:11:48 -0700

    initialized the restfull routes index new create show

---
+++ dbc_overflow/app/controllers/question_controller.rb
@@ -0,0 +1,24 @@
+class QuestionController < ApplicationController
+  def new
+    @question = Question.new
+  end
+
+  def create
+     @question = Question.new(question_params)
+     @question.save
+    #  if @question.save
+    #   status 200
+    # else
+    #   status 404
+    # end
+  end
+
+  def show
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title,:content)
+  end
+end
