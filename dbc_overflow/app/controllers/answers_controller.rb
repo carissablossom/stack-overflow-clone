86b9ff105d4e76befac5089cbc83f8c709278d46
coderica & Caitlyn <pair+coderica.heycait@devbootcamp.com>
2015-05-18 18:14:57 -0700

    add answer form to add new answers to a specific question

--- dbc_overflow/app/controllers/answers_controller.rb
+++ dbc_overflow/app/controllers/answers_controller.rb
@@ -1,10 +1,20 @@
 class AnswersController < ApplicationController
   def new
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
   end
 
   def create
+    @answer = Answer.new(answer_params)
+    @answer.save
   end
 
   def show
+    @answer = Answer.find(params[:id])
+  end
+
+  private
+  def answer_params
+    params.required(:answer).permit(:title, :content, :question_id)
   end
 end
--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -1,10 +1,25 @@
 class QuestionsController < ApplicationController
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+    @question.save
+
+    redirect_to '/questions'
   end
 
   def show
+    @question = Question.find(params[:id])
+  end
+
+  def index
+    @questions = Question.all
+  end
+
+  private
+  def question_params
+    params.required(:question).permit(:title, :content)
   end
 end
