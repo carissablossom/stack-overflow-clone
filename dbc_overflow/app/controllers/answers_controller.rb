bf2ab75bc965872a4c737d3b8e1e52bb53609798
Edwin Unger & Hugh Sutton-Gee <pair+edwinunger.hsuttong@devbootcamp.com>
2015-05-18 18:53:21 -0700

    Release 1

--- dbc_overflow/app/controllers/answers_controller.rb
+++ dbc_overflow/app/controllers/answers_controller.rb
@@ -1,10 +1,27 @@
 class AnswersController < ApplicationController
   def new
+    @answer = Answer.new
   end
 
   def create
+    # @answer = Answer.new(answer_params)
+    #@answer.save
+    @answer = Answer.create(
+      title: params[:title],
+      content: params[:content],
+      question_id: params[:id])
   end
 
   def show
   end
+
+  private
+  # def answer_params
+  #   params.require(:answer).permit(
+  #         :title,
+  #         :content,
+  #         :question_id
+  #         )
+  # end
+
 end
