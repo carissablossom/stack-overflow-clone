c40ebe926cc035efc6b9e2e021d4ac2d5fcbf396
Raghu & Sean Sassenrath <pair+e47my54uc3.seansassenrath@devbootcamp.com>
2015-05-19 10:32:58 -0700

    Update mystery solved

--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   def index
+    @question = Question.new
     @questions = Question.all
   end
 
@@ -15,10 +16,28 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.where(id: params[:id]).first
+    @answer = Answer.new
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to :action => 'index'
+  end
+
+  def edit
+    @question = Question.where(id: params[:id]).first
+  end
+
+  def update
+    @question = Question.where(id: params[:id]).first
+    if @question.update_attributes(post_params)
+      redirect_to :action => "show", :id => params[:id].to_i
+    else
+      redirect_to :action => "edit", :id => params[:id].to_i
+    end
   end
 
   private
-
   def post_params
     params.require(:question).permit(:title, :content)
   end
