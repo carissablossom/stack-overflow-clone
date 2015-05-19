e2bd539dd7aa63b83fa8ad4003b76c1e77c69df8
Darwin D Wu & jackzampolin <pair+darwin67.jackzampolin@devbootcamp.com>
2015-05-18 17:35:21 -0700

    Done with release 2

--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
 
   def index
+    @question = Question.new
     @questions = Question.all
   end
 
@@ -8,6 +9,39 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     @answers = @question.answers
+    @answer = @question.answers.build
   end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      redirect_to 'show'
+    end
+  end
+
+  def destroy
+    Question.find(params[:id]).destroy
+    redirect_to questions_path
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(question_params)
+      redirect_to @question
+    end
+  end
+
+
+  private
+
+    def question_params
+      params.require(:question).permit(:title, :content)
+    end
 
 end
