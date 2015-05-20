4194b0b907f2019eb5974611f220c2a5fc898a42
coderica & Caitlyn <pair+coderica.heycait@devbootcamp.com>
2015-05-20 11:00:00 -0700

    add files we forgot to yesterday

---
+++ dbc_overflow/app/assets/javascripts/questions.js
@@ -0,0 +1,20 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function(){
+  $('.new_question').on('submit', function(e) {
+    e.preventDefault();
+    var $data = $(this).serialize();
+    var $form = $(this);
+    var request = $.ajax({
+      method: "post",
+      url: $(this).attr("action"),
+      data: $data
+    });
+    request.done(function(response){
+      $('#question_list').prepend(response);
+      $form[0].reset();
+    })
+  })
+})
