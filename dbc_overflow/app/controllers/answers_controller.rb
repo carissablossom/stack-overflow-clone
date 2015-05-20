7a01f956f63306aeb2b2902289b3e8340ca71cf7
Joyce De La Torre & RAAAWWRRR <pair+joycedelatorre.yon123@devbootcamp.com>
2015-05-19 17:39:31 -0700

    add views and controllers

--- dbc_overflow/app/controllers/answers_controller.rb
+++ dbc_overflow/app/controllers/answers_controller.rb
@@ -4,21 +4,26 @@ class AnswersController < ApplicationController
   end
   def create
     @answer = Answer.new(answer_params)
+    @answer.question_id = params[:question_id]
+    @answer.save
+
+    redirect_to "/questions/#{params[:question_id]}"
   end
   def new
     @answer = Answer.new
   end
 
   def show
+
   end
 
   private
 
   def answer_params
-    params.require(:answers).permit(
-    title: params[:title],
-    content: params[:content],
-    question_id: params[:question_id]
-      )
+    params.require(:answer).permit(
+    :title,
+    :content,
+    :question_id)
+
   end
 end
--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   def index
-    @question = Question.all
+    @questions = Question.all
     # @question = Question.new
   end
 
@@ -9,8 +9,8 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new(question_params)
-    @question.save
+    question = Question.new(question_params)
+    question.save
     redirect_to questions_path
   end
 
@@ -21,13 +21,26 @@ class QuestionsController < ApplicationController
     # @answers = @question.answers
   end
 
+  def update
+    @question= Question.find(params[:id])
+    @question.update_attributes(question_params)
+    redirect_to :action => :show
+  end
 
   def edit
+    @question = Question.find(params[:id])
+    render "edit"
+  end
+
+  def destroy
+    question = Question.find(params[:id])
+    question.destroy
+    redirect_to root_path
   end
 
 private
   def question_params
-    params.require(:questions).permit(
+    params.require(:question).permit(
       :title,
       :content)
   end
