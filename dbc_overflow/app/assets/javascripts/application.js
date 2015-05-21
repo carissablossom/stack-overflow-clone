00975cd0df2377726d6cbbdd3a6f550be1e85db4
Derek Tang & Charlie Ward <pair+derekztang.wardch@devbootcamp.com>
2015-05-21 15:10:34 -0700

    updated

--- dbc_overflow/app/assets/javascripts/application.js
+++ dbc_overflow/app/assets/javascripts/application.js
@@ -36,63 +36,11 @@ $(document).ready(function(){
     }).fail(function(response){
       console.log(response)
     })
+
+
+  // $('#answer_title').keyup(function(){
+  //   debugger
+  //   // $('#answer.text').css()
+
+  // })
   })
-
-
-$('#question_title').on('keyup', function(event){
-    var input = $('#question_title').val();
-    var myString = input;
-    var regEx = /([^a-zA-Z0-9]){1,}/;
-    var rest = /([a-zA-Z0-9\s\d]{1,})/;
-    var match = regEx.exec(myString);
-    var theRest = rest.exec(input);
-    var count = 0;
-
-    var markdown_1 = match[0]
-    var markdown_2 = theRest[0]
-
-    var h1 = $('<h1></h1>')
-    var h2 = $('<h6></h6>')
-    var ul = $('<ul></ul>')
-    var blockquote = $('<blockquote></blockquote>')
-
-
-
-      if (markdown_1 === "#") {
-        var tag = h1
-        h1.append(markdown_2);
-        $('.markdown_line').html(h1);
-        count+=1 ;
-      } else if (markdown_1 === "##") {
-          var tag = h2
-          h2.append(markdown_2);
-          $('.markdown_line').html(h2);
-          count+=1 ;
-      } else if (markdown_1 === "*" || markdown_1 === "-" ) {
-          var tag = ul
-          ul.append(markdown_2);
-          $('.markdown_line').html(ul);
-          count+=1;
-      } else if (markdown_1 === ">") {
-          var tag = blockquote
-          blockquote.append(markdown_2);
-          $('.markdown_line').html(blockquote);
-          count+=1 ;
-      } else {
-          if (count !== 0) {
-            tag.append(markdown_2);
-            $('.markdown_line').html(tag);
-          } else if (count === 0) {
-            $('.markdown_line').html(markdown_2);
-          }
-        }
-      });
-
-
-
-
-
-
-  })
-
-
--- dbc_overflow/app/models/question.rb
+++ dbc_overflow/app/models/question.rb
@@ -1,4 +1,11 @@
 
 class Question < ActiveRecord::Base
   has_many :answers
+
+  before_save :default_vote_count
+
+  def default_vote_count
+    self.vote_count ||= 0
+  end
+
 end
