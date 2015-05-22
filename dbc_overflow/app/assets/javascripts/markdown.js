70de7329430f7f0f51a5bd6083c3bcec16a0ee38
Hugh Sutton-Gee & Tom Zellner <pair+hsuttong.tomzellner@devbootcamp.com>
2015-05-21 17:25:47 -0700

    add markdown js stub

---
+++ dbc_overflow/app/assets/javascripts/markdown.js
@@ -0,0 +1,12 @@
+//this is our markdown function
+//input: string with markdown syntax
+//output: string with html syntax
+### this is text ###
+<h3> this is thext
+
+
+var markdown = function(s) {
+
+
+return s;
+};
