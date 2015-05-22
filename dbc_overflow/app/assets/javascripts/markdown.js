7eb3874dcc3d9ba18515d520894bcb196a1f5e1a
Hugh Sutton-Gee <hsuttong@gmail.com>
2015-05-21 20:36:36 -0700

    Ignore elasticbeanstalk settings

--- dbc_overflow/app/assets/javascripts/markdown.js
+++ dbc_overflow/app/assets/javascripts/markdown.js
@@ -1,12 +1,3 @@
 //this is our markdown function
 //input: string with markdown syntax
 //output: string with html syntax
-### this is text ###
-<h3> this is thext
-
-
-var markdown = function(s) {
-
-
-return s;
-};
