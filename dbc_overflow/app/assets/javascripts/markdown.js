abcaa4a4f6d5c0c4a4f0893a4b253866b43dccf4
Campbell Belden & Markinator <pair+Campbellb.Makdash49@devbootcamp.com>
2015-05-21 19:25:24 -0700

    Got the bold and italics working

--- dbc_overflow/app/assets/javascripts/markdown.js
+++ dbc_overflow/app/assets/javascripts/markdown.js
@@ -2,13 +2,23 @@
 //   return string.replace(/(\*\*|__)(.*?)\1/, "IT WORKS");
 // }
 $(document).ready(function(){
-   $( "#markdown" ).keyup(function() { //whenever a key is pressed within the #markdown box
-      $("#markdown_text").text($('#markdown').val()); //automatically updates markdown display
-      // $("#markdown_text").html($('#markdown').val().replace(/(\*)+([^.*?$]+)+(\*)/gi,
-      //   "<i>" + "$2"+ "</i>")); //italics
-      $("#markdown_text").html($('#markdown').val().replace(/(\*\*)+([^.*?$]+)+(\*\*)/gi,
-        "<b>" + "$2"+ "</b>")); //sets logic for bold replacement
+  // On document ready runs this function
 
+   $( "#markdown" ).keyup(function() { //whenever a key is pressed within the #markdown box
+      $("#markdown_text").text($('#markdown').val());
+      // Selects the #markdown which is on the left.  Listens for keyup and on key up does this
+      // function.  The function grabs the markdown text class on the right and gives it the
+      // text value from the markdown on the left.
+
+
+
+       //automatically updates markdown display
+      $("#markdown_text").html($('#markdown').val().replace(/(\*\*)+([^.*?$]+)+(\*\*)/gi,
+        "<b>" + "$2"+ "</b>").replace(/(\*)+([^.*?$]+)+(\*)/gi,
+        "<i>" + "$2"+ "</i>")); //sets logic for bold replacement
+      // Now this selects the markdown text on the right and gives it the value of the markdown on the
+      // right once it has had the middle value found by the regex expression replaced with a
+      // html things.  Why can't you chain to the end of this?
         });
 });
 
