8c37503e5773427adc2cb61faa3d4c418477d076
Dudenator <markdmcq@gmail.com>
2015-05-25 21:47:05 -0700

    edit and delete question with rails link_to

--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -52,7 +52,7 @@ class QuestionsController < ApplicationController
     private
 
     def strong_params
-      params.require(:question).permit(:title, :content)
+      params.require(:question).permit(:title, :content, :id)
     end
 
 end
