e981d42ce856160eae34160dcb945f072e5e89b7
Dudenator <markdmcq@gmail.com>
2015-05-25 20:16:09 -0700

    save new questions

--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -2,11 +2,27 @@ class QuestionsController < ApplicationController
 
   def index
     @questions = Question.all
+    @question = Question.new
   end
 
   def show
     @question = Question.find(params[:id])
     @answers = @question.answers
   end
+
+  def create
+    @question = Question.new(strong_params)
+    if @question.save
+      p "Great! It saved!"
+    else
+      p "Shit it didn't save!"
+    end
+  end
+
+    private
+
+    def strong_params
+      params.require(:question).permit(:title, :content)
+    end
 
 end
