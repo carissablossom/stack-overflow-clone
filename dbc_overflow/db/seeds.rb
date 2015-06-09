b025334aad7174f5164146ef74fcc659cf995068
Mary Wallace <wallaceemee@gmail.com>
2015-06-08 18:22:33 -0700

    creating the questions and answers

--- dbc_overflow/db/seeds.rb
+++ dbc_overflow/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+questions = Question.create([
+  { title: 'Is Conch Shell pronounced "contch" or "conque"?', 
+    content: 'I have a friend who seems to pronounce conch like "contch".. Is that correct?' },
+  { title: 'Why is Jerome such a dingleberry?', 
+    content: 'My friend Jerome is sensitive and weird... would you consider him a db?' },
+  { title: 'How many times is it possible to be struck by lightening without dying?', 
+    content: 'My friend has been struck by lightening seven times.. and he is still alive' },
+  { title: 'What is the best programming language to start learning?', 
+    content: 'I want to start programming, but I do not know the best language to learn' },
+  { title: 'How do you create an index file in rails?', 
+    content: '' },
+  { title: 'What are the steps for setting up rails?', 
+    content: '' },
+  { title: 'Why Rails?', 
+    content: '' },
+  { title: 'How can I further break Jacob\'s toe?', 
+    content: '' },
+  { title: 'Is it Action Hank or Hector?', 
+    content: '' },
+  { title: 'What is the best cohort ever?', 
+    content: '' },
+  { title: 'Why are we still getting sandwiches from Specialties?', 
+    content: '' }])
+
+1.'It is properly pronounced "conque", sorry Jacob.'
+2.'Nobody knows.' 
+3.'45% of people have been struck by lightening!'
+
