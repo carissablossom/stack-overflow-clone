185320ae12c78abee28b97f0434777d517af0719
banned4hax & Jerome Chenette <pair+banned4hax.jeromechenette@devbootcamp.com>
2015-06-08 21:40:14 -0700

    start release 2

--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -1,10 +1,24 @@
 class QuestionsController < ApplicationController
   def index
-       @questions= Question.all
+    @questions= Question.all
+    @question = Question.new
   end
 
   def show
     @question = Question.find(params[:id])
-    @answers = @question.answers
+    @answers =  @question.answers
+
+  end
+
+  def create
+    @question = Question.new(question_params)  #question_params is a method will make private
+
+  @question.save
+  redirect_to questions_path
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :content)
   end
 end
