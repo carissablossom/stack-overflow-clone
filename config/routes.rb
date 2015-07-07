a1d958c74269c0bf7e330dde651371916cb6b3b6
Arielle Chen & Tanay Arora <pair+achen116.tarora2014@devbootcamp.com>
2015-07-06 18:17:05 -0700

    run db:migrate for answers and start working on user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
   resources :questions
+  resources :users
+
+  # get '/users'
+  # get 'signup' => 'users#new'
+  # post 'users#create'
+  get '/signin' => 'users#login'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
