7eecda9ba5d756aee8f36fa9e38a546f003dd001
achen116 <chen.arielle@gmail.com>
2015-07-07 00:01:45 -0700

    add signin/signup forms and links, add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+  	@user = User.where(username: params[:session][:username]).first
+
+  	if @user && (@user.password == params[:session][:password])
+  		session[:user_id] = @user.id
+  		redirect_to 'questions_path'
+  	else
+  		@signin_error = "Username/Password invalid. Please try again."
+  		# checkout Hartl Ch.8 for implementing flash msgs to catch session errors
+  		render 'new'
+  	end
+  end
+
+  def destroy
+		session[:user_id] = nil
+
+  	redirect_to 'questions_path'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   resources :questions do
     resources :answers
   end
 
-  # resources :users
-  # get '/signup' => 'users#new'
+  resources :users
+  get 'signup' => 'users#new'
+
+  get 'signin' => 'sessions#new'
+  post 'signin' => 'sessions#create'
+  delete 'signout' => 'sessions#destroy'
 end
