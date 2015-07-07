5befb78e9362ee6b5fab43fc9dea66e90579baa5
Arielle Chen & Tanay Arora <pair+achen116.tarora2014@devbootcamp.com>
2015-07-06 19:22:52 -0700

    edit question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -22,6 +22,20 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(question_params)
+      redirect_to question_path(@question)
+    else
+      @update_errors = @question.errors.full_messages
+      render 'edit'
+    end
+  end
 
   private
 
