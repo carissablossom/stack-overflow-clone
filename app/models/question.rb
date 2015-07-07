9ebc09c781b471a7d7ad7683b4a9dc671da57d69
Arielle Chen & Tanay Arora <pair+achen116.tarora2014@devbootcamp.com>
2015-07-06 17:15:37 -0700

    add answers model and migration

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :user
+  has_many   :answers
 
   validates :title, presence: true, uniqueness: true
   validates :content, presence: true, uniqueness: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :questions
+  has_many :answers
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: {minimum: 6}
