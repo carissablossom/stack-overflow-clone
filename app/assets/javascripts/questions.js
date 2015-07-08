81386aea102a9719d57e091d46824584d14a0ea2
Arielle Chen & Le Tran <pair+achen116.ltran1231@devbootcamp.com>
2015-07-08 16:16:52 -0700

    add comments

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -8,8 +8,9 @@ $(document).on('ajax:error', '.add-question-link', function(event) {
   console.log('load question form fail', arguments);
 });
 
+// append new question
 $(document).on('ajax:success', '.new_question', function(event, response, status, xhr) {
-  $('body').append(response);
+  $('body').append(response); //needs to be cleaned up
 });
 
 $(document).on('ajax:error', '.new_question', function(event, response, status, xhr) {
